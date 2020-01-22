bd640f7ef6804800238bcd6adb5ba4c38e247192
Raz <razsh98@gmail>
2020-01-22 19:58:23 +0200

    Written skeleton for remaining test classes

---
+++ test/TopDown/MinValueTopDown.java
@@ -0,0 +1,21 @@
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MinValueTopDown {
+
+    @BeforeClass
+    public static void init(){
+
+    }
+
+    @Before
+    public void initTest(){
+    }
+
+    @AfterClass
+    public static void finish(){
+
+    }
+}
